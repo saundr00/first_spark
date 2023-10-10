c8369dd535ab4de564935cf0d1853a689f8f310a
Bobby Saunders <mail@bobbysaunders.coms>
2023-10-10 13:12:34 -0400

    .

--- db_init/init_02.sh
+++ db_init/init_02.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /var/lib/postgres/var/lib/postgres/apache-hive-3.1.2-bin/bin
+cd /var/lib/postgres/apache-hive-3.1.2-bin/bin
 ./schematool -dbType postgres -driver org.postgresql.Driver \
 -url jdbc:postgresql://localhost:5432/metastore_db \
 -userName postgres -passWord postgres -initSchema
