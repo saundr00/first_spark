2f5c7ff749682e3293a31922cb3326dde9799102
Bobby Saunders <mail@bobbysaunders.coms>
2023-10-10 13:20:22 -0400

    .

--- db_init/init_02.sh
+++ db_init/init_02.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+export HADOOP_HOME=/var/lib/postgres/hadoop-3.3.6
+export HIVE_HOME=/var/lib/postgres/apache-hive-3.1.2-bin
+export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-arm64
+
 cd /var/lib/postgres/apache-hive-3.1.2-bin/bin
 ./schematool -dbType postgres -driver org.postgresql.Driver \
 -url jdbc:postgresql://localhost:5432/metastore_db \
